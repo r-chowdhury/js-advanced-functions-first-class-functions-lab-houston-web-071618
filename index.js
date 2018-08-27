3459e8200128eda0197e3a15aa56548be165f7f3
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-08-27 14:08:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+  return [drivers[0], drivers[1]]
+}
+
+const returnLastTwoDrivers = (drivers) => {
+  return [drivers[drivers.length-2], drivers[drivers.length-1]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (fare) => {
+  return MuliplyFare = (multiple) => {
+    return fare * multiple
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, whichDrivers) => {
+  return whichDrivers(drivers)
+}
